5a7eb4cf1992c56f94fab3e2dfa05bbc9b1ce47b
jtompkins1 <jennifermtompkins@gmail.com>
2026-01-18 19:28:25 -0600

    contacts part 2

--- routes/contacts.js
+++ routes/contacts.js
@@ -9,4 +9,12 @@ router.get('/', contactsController.getAllContacts);
 
 router.get('/:id', contactsController.getContactById);
 
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+
+
+
 module.exports = router;
